d77de995bc7f09b9fb2d984d16679394ee21a6ba
spiff-radio <hello.spiff.radio@gmail.com>
2023-06-01 13:20:09 +0200

    playlist find / findOne middlewares

--- src/api/playlist/routes/playlist.js
+++ src/api/playlist/routes/playlist.js
@@ -6,4 +6,13 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::playlist.playlist');
+module.exports = createCoreRouter('api::playlist.playlist', {
+ config: {
+   find: {
+     middlewares: ['plugin::spiff-api.playlist-find']
+   },
+   findOne: {
+     middlewares: ['plugin::spiff-api.playlist-find-one']
+   },
+ },
+})
