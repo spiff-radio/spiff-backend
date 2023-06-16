21eadb01203104b03e80e880fba88e71102fcd85
spiff-radio <hello.spiff.radio@gmail.com>
2023-06-16 10:31:51 +0200

    automatically set .author and .publishedAt when creating a playlist

--- src/api/playlist/controllers/playlist.js
+++ src/api/playlist/controllers/playlist.js
@@ -6,4 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::playlist.playlist');
+module.exports = createCoreController('api::playlist.playlist', ({ strapi }) => ({
+
+  //https://strapi.gitee.io/documentation/v3.x/guides/is-owner.html
+  async create(ctx) {
+    const postData = ctx.request.body.data;
+    const user = ctx.state.user;
+
+    if (!user){
+      return ctx.unauthorized();
+    }
+
+    //automatically set logged user as author of the entity
+    postData.author = user.id;
+
+    //set publishedAt (= non-draft post)
+    postData.publishedAt = new Date().getTime();
+
+    //update context
+    //strapi.entityService.create("api::playlist.playlist",{data:postData})
+    ctx.request.body.data = postData;
+
+    return await super.create(ctx);
+  },
+}));
