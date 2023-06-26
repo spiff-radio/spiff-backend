edb315233bedeebc6a5515e2497466c41c6b258a
spiff-radio <hello.spiff.radio@gmail.com>
2023-06-26 10:56:12 +0200

    minor

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),// For self-signed certificates
       },
     },
     debug: false,
