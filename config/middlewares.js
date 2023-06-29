09301806dad6820fa5be4b90c128b5e9137af723
spiff-radio <hello.spiff.radio@gmail.com>
2023-06-29 13:28:45 +0200

    strapi4-ftp-provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,29 @@
 module.exports = ({ env }) => ([
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            env('FTP_BASE_URL'),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            env('FTP_BASE_URL'),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,18 @@ module.exports = ({ env }) => ({
     config: {
       jwtSecret: env('JWT_SECRET')
     },
-  }
+  },
+  upload: {
+    config: {
+      provider: "strapi4-ftp-provider",
+      providerOptions: {
+        host: env("FTP_HOST"),
+        port: env("FTP_PORT",21),
+        user: env("FTP_USER"),
+        password: env("FTP_PASSWORD"),
+        path: env("FTP_BASE_PATH"),
+        baseUrl: env("FTP_BASE_URL"),
+      },
+    },
+  },
 })
