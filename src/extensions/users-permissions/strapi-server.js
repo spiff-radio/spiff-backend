936b601bc37a3d7dabd79dfb638f190dce9f3d76
spiff-radio <hello.spiff.radio@gmail.com>
2023-07-10 13:25:09 +0200

    deps update + spotify WIP

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -23,39 +23,39 @@ module.exports = (plugin) => {
       return route;
     });
 
-    //update route GET /users/:id
-    apiRoutes
-      .filter(route => route.handler === 'user.findOne')
-      .map(route => {
-        route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-find-one'];
-        return route;
-      });
+  //update route GET /users/:id
+  apiRoutes
+    .filter(route => route.handler === 'user.findOne')
+    .map(route => {
+      route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-find-one'];
+      return route;
+    });
 
-    //update route GET /users
-    apiRoutes
-      .filter(route => route.handler === 'user.find')
-      .map(route => {
-        route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-find'];
-        return route;
-      });
+  //update route GET /users
+  apiRoutes
+    .filter(route => route.handler === 'user.find')
+    .map(route => {
+      route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-find'];
+      return route;
+    });
 
-    //update route PUT /users/:id
-    apiRoutes
-      .filter(route => route.handler === 'user.update')
-      .map(route => {
-        route.config.policies = [...(route.config.policies || []), 'plugin::users-permissions.is-owner'];
-        route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-update'];
-        return route;
-      });
+  //update route PUT /users/:id
+  apiRoutes
+    .filter(route => route.handler === 'user.update')
+    .map(route => {
+      route.config.policies = [...(route.config.policies || []), 'plugin::users-permissions.is-owner'];
+      route.config.middlewares = [...(route.config.middlewares || []), 'plugin::spiff.user-update'];
+      return route;
+    });
 
-    //update route DELETE /users/:id
-    apiRoutes
-      .filter(route => route.handler === 'user.destroy')
-      .map(route => {
-        route.config.policies = [...(route.config.policies || []), 'plugin::users-permissions.is-owner'];
-        return route;
-      });
+  //update route DELETE /users/:id
+  apiRoutes
+    .filter(route => route.handler === 'user.destroy')
+    .map(route => {
+      route.config.policies = [...(route.config.policies || []), 'plugin::users-permissions.is-owner'];
+      return route;
+    });
 
-    return plugin;
+  return plugin;
 
 };
